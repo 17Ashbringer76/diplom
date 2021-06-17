2fd7645f33ade2d59bcf6335202bcd2372a7dcb9
17Ashbringer76 <77210553+17Ashbringer76@users.noreply.github.com>
2021-06-17 18:32:32 +0300

    Added Data files, page files and tests

--- src/test/java/ru/netology/data/DataHelper.java
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -1,4 +1,219 @@
 package ru.netology.data;
 
+import com.github.javafaker.Faker;
+
+import java.time.Year;
+import java.util.Locale;
+import java.util.Random;
+
+import static java.lang.Integer.parseInt;
+
 public class DataHelper {
-}
+    private static Faker faker = new Faker();
+    private static Faker fakerRu = new Faker(new Locale("ru"));
+
+    private static String approvedCard = "4444 4444 4444 4441";
+    private static String declinedCard = "4444 4444 4444 4442";
+
+    private static String generateMonth() {
+        Random random = new Random();
+        int num = random.nextInt(12) + 1;
+        if (num < 10) {
+            return String.format("%02d", num);
+        } else {
+            return String.valueOf(num);
+        }
+    }
+        private static String generateYear() {
+            Random random = new Random();
+            int fullYear = Year.now().getValue();
+            String stringFullYear = String.valueOf(fullYear).substring(2,4);
+            int year = parseInt(stringFullYear, 10) + random.nextInt(4) + 1;
+            return String.valueOf(year);
+        }
+
+        /* 1 */
+        public static Card getValidCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 2 */
+        public static Card getEmptyFieldsCardInfo() {
+            return new Card("",
+                    "",
+                    "",
+                    "",
+                    "");
+        }
+        /* 3 */
+        public static Card getEmptyCardNumberFieldCardInfo() {
+            return new Card("",
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 4 */
+        public static Card getEmptyMonthFieldCardInfo() {
+            return new Card(approvedCard,
+                    "",
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 5 */
+        public static Card getEmptyYearFiledCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    "",
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 6 */
+        public static Card getEmptyCardholderFiledCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    "",
+                    faker.numerify("###"));
+        }
+        /* 7 */
+        public static Card getEmptyCVVFieldCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    "");
+        }
+        /* 8 */
+        public static Card getInvalidCardNumberCardInfo() {
+            return new Card("5111111133337777",
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 9 */
+        public static Card getNonexistentCardNumberCardInfo() {
+            return new Card("3333444455558888",
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 10 */
+        public static Card getIncompleteCardNumberCardInfo() {
+            return new Card("444444444444444",
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 11 */
+        public static Card getZerosInMonthFiledCardInfo() {
+            return new Card(approvedCard,
+                    String.format("%02d", 0),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 12 */
+        public static Card getNonexistentMonthCardInfo() {
+            return new Card(approvedCard,
+                    "15",
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 13 */
+        public static Card getMothWithoutZeroFirstCharacterCardInfo() {
+            return new Card(approvedCard,
+                    "9",
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 14 */
+        public static Card getZeroInYearFieldCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    "0",
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 15 */
+        public static Card getExpiredCardCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    "19",
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 16 */
+        public static Card getInvalidYearCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    "99",
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 17 */
+        public static Card getCardholderOnlyFirstNameCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 18 */
+        public static Card getLongCardholderNameCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    "TRENT ALEKSANDR-ARNOLD",
+                    faker.numerify("###"));
+        }
+        /* 19 */
+        public static Card getCyrillicCardholderNameCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    fakerRu.name().firstName().toUpperCase() + " " + fakerRu.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 20 */
+        public static Card getSpecialCharactersInCardholderNameCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + "~!@#$%^&*()_-+=,./?><|" + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("###"));
+        }
+        /* 21 */
+        public static Card getNumbersInCardholderNameCardInfo() {
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + "1122334455" + " " + faker.name().lastName().toUpperCase() + "67890",
+                    faker.numerify("###"));
+        }
+        /* 22 */
+        public static Card getOneNumberCVVCardInfo(){
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("#"));
+        }
+        /* 23 */
+        public static Card getTwoNumbersCVVCardInfo(){
+            return new Card(approvedCard,
+                    generateMonth(),
+                    generateYear(),
+                    faker.name().firstName().toUpperCase() + " " + faker.name().lastName().toUpperCase(),
+                    faker.numerify("##"));
+        }
+    }
--- src/test/java/ru/netology/test/PaymentByCardTest.java
+++ src/test/java/ru/netology/test/PaymentByCardTest.java
@@ -1,4 +1,273 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import lombok.val;
+import org.junit.jupiter.api.*;
+import ru.netology.page.StartPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataHelper.*;
+import static ru.netology.data.SQLHelper.*;
+
 public class PaymentByCardTest {
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    void setUpEach() {
+        open(System.getProperty("sut.url"));
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @AfterEach
+    void clearDataBase() {
+        clearDB();
+    }
+
+    /* 1 */
+    @Test
+    void validCardPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getValidCardInfo());
+        paymentByCardPage.successMsgWait();
+        paymentByCardPage.successMsgClose();
+        assertEquals("APPROVED", getPaymentByCardStatus());
+    }
+
+    /* 2 */
+    @Test
+    void allFieldsEmptyPaymentByCardTest () {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getEmptyFieldsCardInfo());
+        paymentByCardPage.cardNumberBadFormatErrorWait();
+        paymentByCardPage.monthBadFormatErrorWait();
+        paymentByCardPage.yearBadFormatErrorWait();
+        paymentByCardPage.cardholderRequiredErrorWait();
+        paymentByCardPage.cvvBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 3 */
+    @Test
+    void cardNumberFieldEmptyPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getEmptyCardNumberFieldCardInfo());
+        paymentByCardPage.cardNumberBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 4 */
+    @Test
+    void monthFiledEmptyPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getEmptyMonthFieldCardInfo());
+        paymentByCardPage.monthBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 5 */
+    @Test
+    void yearFieldEmptyPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getEmptyYearFiledCardInfo());
+        paymentByCardPage.yearBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 6 */
+    @Test
+    void cardholderFiledEmptyPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getEmptyCardholderFiledCardInfo());
+        paymentByCardPage.cardholderRequiredErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 7 */
+    @Test
+    void cvvFieldEmptyPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getEmptyCVVFieldCardInfo());
+        paymentByCardPage.cvvBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 8 */
+    @Test
+    void invalidCardNumberPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getInvalidCardNumberCardInfo());
+        paymentByCardPage.declineMsgWait();
+        paymentByCardPage.declineMsgClose();
+        assertEquals("DECLINED", getPaymentByCardStatus());
+    }
+
+    /* 9 */
+    @Test
+    void nonexistentCardNumberPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getNonexistentCardNumberCardInfo());
+        paymentByCardPage.cardNumberBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 10 */
+    @Test
+    void incompleteCardNumberPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getIncompleteCardNumberCardInfo());
+        paymentByCardPage.cardNumberBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 11 */
+    @Test
+    void zerosInMonthFiledPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getZerosInMonthFiledCardInfo());
+        paymentByCardPage.monthBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+
+    /* 12 */
+    @Test
+    void nonexistentMonthPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getNonexistentMonthCardInfo());
+        paymentByCardPage.monthInvalidExpirationDateErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 13 */
+    @Test
+    void mothWithoutZeroFirstCharacterPaymentByCardTest () {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getMothWithoutZeroFirstCharacterCardInfo());
+        paymentByCardPage.monthBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 14 */
+    @Test
+    void zeroInYearFieldPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getZeroInYearFieldCardInfo());
+        paymentByCardPage.yearBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 15 */
+    @Test
+    void expiredCardPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getExpiredCardCardInfo());
+        paymentByCardPage.yearCardExpiredErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 16 */
+    @Test
+    void invalidYearPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getInvalidYearCardInfo());
+        paymentByCardPage.yearInvalidExpirationDateErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 17 */
+    @Test
+    void cardholderOnlyFirstNamePaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getCardholderOnlyFirstNameCardInfo());
+        paymentByCardPage.successMsgWait();
+        paymentByCardPage.successMsgClose();
+        assertEquals("APPROVED", getPaymentByCardStatus());
+    }
+
+    /* 18 */
+    @Test
+    void longCardholderNamePaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getLongCardholderNameCardInfo());
+        paymentByCardPage.successMsgWait();
+        paymentByCardPage.successMsgClose();
+        assertEquals("APPROVED", getPaymentByCardStatus());
+    }
+
+    /* 19 */
+    @Test
+    void cyrillicCardholderNamePaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getCyrillicCardholderNameCardInfo());
+        paymentByCardPage.cardNumberBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 20 */
+    @Test
+    void specialCharactersInCardholderNamePaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getSpecialCharactersInCardholderNameCardInfo());
+        paymentByCardPage.cardNumberBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 21 */
+    @Test
+    void numbersInCardholderNamePaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getNumbersInCardholderNameCardInfo());
+        paymentByCardPage.cardNumberBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 22 */
+    @Test
+    void oneNumberCVVPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getOneNumberCVVCardInfo());
+        paymentByCardPage.cvvBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
+
+    /* 23 */
+    @Test
+    void twoNumbersCVVPaymentByCardTest() {
+        val startPage = new StartPage();
+        val paymentByCardPage = startPage.selectPaymentByCardPage();
+        paymentByCardPage.inputData(getTwoNumbersCVVCardInfo());
+        paymentByCardPage.cvvBadFormatErrorWait();
+        assertEquals("NO DATA", getPaymentByCardStatus());
+    }
 }
